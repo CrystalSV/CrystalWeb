722b5911060e44e96fe8b170fe538f562c3f5461
Waldo Enrique Ramirez G. <blak.fire.exe@gmail.com>
2013-03-12 14:49:17 -0600

    Agregando Routing y Controladores
    Falta pasarlo a ingles, :P  (Copy-Paste)

---
+++ src/Crystal/CrystalWebBundle/Controller/ProjectsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Crystal\CrystalWebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Crystal\BaseBundle\Entity\catProjects;
+
+class ProjectsController extends Controller
+{
+	public function listarAction()
+	{
+		
+	}
+
+	public function crearAction()
+	{
+		
+	}
+
+	public function editarAction($id)
+	{
+		
+	}
+
+	public function borrarAction($id)
+	{
+		
+	}
+}
